079104ac1da452aa3c2cbdef41b0587fd98cbbdb
Ivan Jimenez <chiklete@gmail.com>
2021-12-19 01:48:14 +0100

    modificando port

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 //Ejecutamos la funcion que conecta con la db
 dbConnection();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 
 const server = express();
 
